a6cbe7f787d342d28d64d2e43456c7b33b4eac27
Raj Raj <raj@cambridgespark.com>
2021-06-20 14:51:29 +0100

    Add navbar componnents and style

---
+++ src/layout/navbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <router-link :to="{ path: '/' }" class="navbar-item">
+          <p>BNT</p>
+        </router-link>
+
+        <a role="button" class="navbar-burger" :class="hamburgerClass" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" @click="toggleHamburger">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div id="navbarBasicExample" class="navbar-menu" :class="hamburgerClass">
+        <div class="navbar-start">
+          <router-link :to="{ path: '/' }" class="navbar-item">
+            Home
+          </router-link>
+        </div>
+      </div>
+    </nav>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    showHamburger: false,
+  }),
+  computed: {
+    hamburgerClass() {
+      return this.showHamburger ? 'is-active' : '';
+    },
+  },
+  methods: {
+    toggleHamburger() {
+      console.log('clicked');
+      this.showHamburger = !this.showHamburger;
+    },
+  },
+}
+</script>
